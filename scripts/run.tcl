8de8e60aa2236bfdcbda5d21c19d7be8c3552063
Nayan Das <nayandas00333@gmail.com>
2026-09-19 11:43:01 +0530

    Add files via upload

---
+++ scripts/run.tcl
@@ -0,0 +1,10 @@
+set root [file dirname [info script]]
+cd $root
+create_project simulation ./project -part xc7z010clg400-1 -force
+add_files [list $root/../../rtl/analytic.v $root/../../rtl/dual_fvc.v]
+add_files -fileset sim_1 $root/tb.sv
+set_property top tb [get_filesets sim_1]
+set_property xsim.simulate.runtime all [get_filesets sim_1]
+launch_simulation
+close_sim
+exit
